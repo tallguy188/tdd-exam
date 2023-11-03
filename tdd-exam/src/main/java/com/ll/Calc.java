a9dec25deebba6b753f7ed9056eb3e2bbe22d1f3
tallguy188 <cksghd4220@naver.com>
2023-11-03 11:45:17 +0900

    Minus Refactoring

--- tdd-exam/src/main/java/com/ll/Calc.java
+++ tdd-exam/src/main/java/com/ll/Calc.java
@@ -2,6 +2,25 @@ package com.ll;
 
 public class Calc {
     public static int run(String exp) {
-        return 20;
+        if(exp.isBlank()) return 0;
+
+        final boolean isPlus = exp.contains("+");
+
+        // +도 정규표현식이라 앞에 역슬래쉬 두개 붙여야함.
+        final String divideSign = isPlus ? "\\+" : "-";
+
+        final String[] expBits = exp.split(" " + divideSign + " " );
+
+        final int num1 = Integer.parseInt(expBits[0]);
+        final int num2 = Integer.parseInt(expBits[1]);
+
+        if (isPlus) {
+
+            return num1 + num2;
+        }
+        else {
+            return num1 - num2;
+        }
+
     }
 }
--- tdd-exam/src/test/java/com/ll/CalcTest.java
+++ tdd-exam/src/test/java/com/ll/CalcTest.java
@@ -8,7 +8,7 @@ public class CalcTest {
     @Test
     @DisplayName("Calc의 run 메서드는 int를 리턴한다.")
     void t1() {
-        int rs = Calc.run("");
+        int rs = Calc.run("10 + 20");
 
     }
 
@@ -21,9 +21,27 @@ public class CalcTest {
     }
 
     @Test
-    @DisplayName("10 + 10 = 20")
+    @DisplayName("10 + 20 = 20")
     void t3() {
+        int rs = Calc.run("10 + 20");
+        assertThat(rs).isEqualTo(30);  // rs가 20이랑 똑같아야한다는 단정문
+    }
+
+
+
+
+    @Test
+    @DisplayName("10 - 10 = 20")
+    void t4() {
         int rs = Calc.run("10 - 10");
         assertThat(rs).isEqualTo(0);  // rs가 20이랑 똑같아야한다는 단정문
     }
+
+    @Test
+    @DisplayName("10 - 5 = 5")
+    void t5() {
+        int rs = Calc.run("10 - 5");
+        assertThat(rs).isEqualTo(5);
+    }
+
 }
